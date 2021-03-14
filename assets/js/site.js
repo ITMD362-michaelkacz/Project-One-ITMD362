7166f302c867a4fb03c764a2eb927c6a44326cc3
Michael Kaczowka <mkaczowka@hawk.iit.edu>
2021-03-13 20:40:42 -0600

    Create function run when submit button is pressed

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,18 @@
+
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function validateForm() {
+  var w = document.forms["kickboxing"]["name"].value;
+  var x = document.forms["kickboxing"]["email"].value;
+  var y = document.forms["kickboxing"]["birthday"].value;
+  var z = document.forms["kickboxing"]["phone"].value
+  if ((w == "")||(x == "")||(y == "")||(z == "")){
+    alert("Please fill in the missing entries");
+  }
+  else {
+    alert("Enjoy your free kickboxing lesson! Details will be sent via email");
+  }
+}
